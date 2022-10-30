410bcdb7b140d8c4645f24eb0fab11b587010fe6
beoma21 <vandrager21@gmail.com>
2022-10-30 14:27:14 +0900

    Starbucks_section_park_221030

--- Starbucks/JS/youtube.js
+++ Starbucks/JS/youtube.js
@@ -7,18 +7,20 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-function onYouTubeIframeAPIReady() {
+function onYouTubePlayerAPIReady() {
   new YT.Player('player', {
-    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상id값 아이디만 넣어서 제어하자 소스코드 다 넣으면 제어 불가
+    videoId: 'An6LvWQuj_8', // 최초 재생할 유튜브 영상 ID
     playerVars: {
-        autoplay: true,
-        loop: true,
-        playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 id 목록
+      autoplay: true, // 자동 재생 유무
+      loop: true, // 반복 재생 유무
+      playlist: 'An6LvWQuj_8' // 반복 재생할 유튜브 영상 ID 목록
     },
     events: {
-        onReady: function (event) {
-            event.target.mute() // 음소거
-        }
+      onReady: function (event) {
+        event.target.mute() // 음소거!
+      }
     }
-  });
+  })
 }
+
+
